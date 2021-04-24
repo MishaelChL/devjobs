c929992aa53ba9db7cbc2265a24e5173fb6c51be
MishaelChL <jose.mishael.chile@gmail.com>
2021-04-24 15:18:32 -0400

    instalando mongoose y conectando a la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const mongoose = require("mongoose");
+require("./config/db");
+
 const express = require("express");
 const routes = require("./routes");
 const exphbs = require("express-handlebars");
-const path = require("path");//sirve para obtener la url actual o de ciertos documentos
+//sirve para obtener la url actual o de ciertos documentos
+const path = require("path");
+//para almacenar los usuarios autenticados
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+//
+const MongoStore = require("connect-mongo")(session);
 
 require("dotenv").config({ path: "variables.env" });
 
@@ -17,6 +26,16 @@ app.set("view engine", "handlebars");
 
 //static files, para leer los archivos estaticos que tenemos en public
 app.use(express.static(path.join(__dirname, "public")));
+
+//
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
 
 app.use("/", routes())
 
