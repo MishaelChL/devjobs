80fb4f64b1b0719bd83f637c023ad34bfeb776d7
MishaelChL <jose.mishael.chile@gmail.com>
2021-04-23 23:52:45 -0400

    habilitando handlebars como template engine

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const routes = require("./routes");
+const exphbs = require("express-handlebars");
+const path = require("path");//sirve para obtener la url actual o de ciertos documentos
+
 
 const app = express();
+
+//habilitar handelbars como view
+app.engine("handlebars", 
+    exphbs({
+        defaultLayout: "layout"
+    })
+);
+app.set("view engine", "handlebars");
+
+//static files, para leer los archivos estaticos que tenemos en public
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes())
 
