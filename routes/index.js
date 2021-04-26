7d71f990b34d02a07a5f5320e46d99bdab7b6388
MishaelChL <jose.mishael.chile@gmail.com>
2021-04-26 18:24:29 -0400

    creando el formulario para crear cuentas

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/homeController");
 const vacantesController = require("../controllers/vacantesController");
+const usuariosController = require("../controllers/usuariosController");
 
 module.exports = () => {
     router.get("/", homeController.mostrarTrabajos);
@@ -16,5 +17,8 @@ module.exports = () => {
     //editar vacante
     router.get("/vacantes/editar/:url", vacantesController.formEditarVacante);
     router.post("/vacantes/editar/:url", vacantesController.editarVacante);
+
+    //crear cuentas
+    router.get("/crear-cuenta", usuariosController.formCrearCuenta);
     return router;
 }
